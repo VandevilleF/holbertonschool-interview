e05700f959ae89c83012f4bdac9f3cfe856ed1c2
VandevilleF <flo.vandeville@gmail.com>
2025-01-21 11:10:44 +0100

    Add comment

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Find the minimum operations to reach for n"""
+
 
 def minOperations(n):
-    """"""
+    """Find the minimum operations to reach for n"""
     operation = 0
     actuel = 1
 
