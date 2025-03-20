4df733d0795cf0c6ec8d2bb1b0a5488abcecaf60
VandevilleF <flo.vandeville@gmail.com>
2025-03-20 14:52:04 +0100

    Betty

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -11,14 +11,14 @@
  */
 char check_char(int row, int col)
 {
-    while (row > 0 || col > 0)
-    {
-        if (row % 3 == 1 && col % 3 == 1)
-            return ' ';
-        row /= 3;
-        col /= 3;
-    }
-    return '#';
+	while (row > 0 || col > 0)
+	{
+		if (row % 3 == 1 && col % 3 == 1)
+			return ' ';
+		row /= 3;
+		col /= 3;
+	}
+	return ('#');
 }
 
 /**
@@ -27,17 +27,17 @@ char check_char(int row, int col)
  */
 void menger(int level)
 {
-    if (level < 0)
-        return;
+	if (level < 0)
+		return;
 
-    int size = pow(3, level);
+	int size = pow(3, level);
 
-    for (int row = 0; row < size; row++)
-    {
-        for (int col = 0; col < size; col++)
-        {
-            putchar(check_char(row, col));
-        }
-        putchar('\n');
-    }
+	for (int row = 0; row < size; row++)
+	{
+		for (int col = 0; col < size; col++)
+		{
+			putchar(check_char(row, col));
+		}
+		putchar('\n');
+	}
 }
