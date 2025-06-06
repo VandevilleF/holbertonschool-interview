31fd17c145e5b3c519e2f99f20da039f938f7e9e
VandevilleF <flo.vandeville@gmail.com>
2025-06-06 19:58:15 +0200

    style: adjust function declaration formatting for consistency

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -7,7 +7,8 @@
  * Return: Address of the node where the loop starts, or NULL if no loop
  */
 
-listint_t *find_listint_loop(listint_t *head) {
+listint_t *find_listint_loop(listint_t *head)
+{
 	listint_t *slow = head, *fast = head;
 
 	if (head == NULL)
