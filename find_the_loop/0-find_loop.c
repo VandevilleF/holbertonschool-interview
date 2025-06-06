e4e47d0e086475ec75d0cab8edf51603f62b4fea
VandevilleF <flo.vandeville@gmail.com>
2025-06-06 19:56:30 +0200

    style: format code for consistency and readability

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -10,15 +10,15 @@
 listint_t *find_listint_loop(listint_t *head) {
 	listint_t *slow = head, *fast = head;
 
-	if(head == NULL)
+	if (head == NULL)
 		return (NULL);
 
-	while(fast != NULL && fast->next != NULL)
+	while (fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 
-		if(slow == fast)
+		if (slow == fast)
 		{
 			slow = head;
 			while (slow != fast)
